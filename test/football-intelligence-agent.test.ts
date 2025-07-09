1edfab892deea89c9b0169311bcb59cfd96a6994
abel <vnponce8@gmail.com>
2025-07-08 23:57:04 -0700

    Initial commit

---
+++ test/football-intelligence-agent.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FootballIntelligenceAgent from '../lib/football-intelligence-agent-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/football-intelligence-agent-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FootballIntelligenceAgent.FootballIntelligenceAgentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
